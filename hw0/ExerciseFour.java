fca7d051c544e9745ad7ede043a08e911e1e07f0
rashadg1030 <rashad.sasaki@gmail.com>
2020-05-21 18:07:54 -0700

    Add 0th homework

---
+++ hw0/ExerciseFour.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    int[] a = {1, 2, -3, 4, 5, 4};
+    int n = 3;
+    windowPosSum(a, n);
+
+    // Should print 4, 8, -3, 13, 9, 4
+    System.out.println(java.util.Arrays.toString(a));
+  }
+
+  /*
+  Write a function windowPosSum(int[] a, int n) that replaces each element a[i] with the sum of a[i] through a[i + n], but only if a[i] is positive valued. If there are not enough values because we reach the end of the array, we sum only as many values as we have.
+  */
+  public static void windowPosSum(int[] a, int n) {
+    for(int i = 0; i < a.length; i++) {
+      if (a[i] < 0)
+        continue;
+
+      int sum = 0;
+      int bound = i + n;
+      for (int j = i; j <= bound; j++) {
+        if (j == a.length) 
+          break;
+        sum+=a[j];
+      }
+
+      a[i] = sum;
+    }  
+  }
+}
